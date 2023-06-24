013e1e90ee53acf116884f1f87dc5f23d7345c39
zins9885 <soojin9885@naver.com>
2023-06-24 17:12:07 +0900

    main css final
    6/24 desing The End!!!!!

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import '../App.css'
 const Footer = () => {
 	return (
+		
 		<div className='footer-container' style={{position:"fixed", bottom:"0px", width:"100%", height: "fit-content", textAlign: "center"}} >
+		<div class="info">
+        <span>사업자등록번호 111-11-1111111</span>
+        <span>(주)모두의캠핑 대표이사 스인재</span>
+        <span>TEL : 062) 3015-1100 / FAX : 062) 333-1166</span>
+        <span>개인정보 책임자 :홍길동</span>
+        </div>
 			Copyright © 2023 Backpackr All right reserved.
 		</div>
 	)
